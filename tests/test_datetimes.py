fa575ae678da8790fdf42c46642f5154175420b0
Sam Park <spark@goodrx.com>
2019-02-08 10:16:06 -0800

    Remove coverage hack

--- tests/test_datetimes.py
+++ tests/test_datetimes.py
@@ -8,7 +8,7 @@ from unittest import SkipTest
 import pytest
 from tests import utils
 
-from freezegun import api, freeze_time
+from freezegun import freeze_time
 from freezegun.api import FakeDatetime, FakeDate
 
 try:
@@ -628,16 +628,6 @@ def test_time_with_nested():
         assert time() == second
 
 
-@pytest.fixture
-def change_stack_limit():
-    ori_val = api.call_stack_inspection_limit
-    api.call_stack_inspection_limit = 100  # just to increase coverage
-    yield
-    # Restore to normal after test(s)
-    api.call_stack_inspection_limit = ori_val
-
-
-@pytest.mark.usefixtures('change_stack_limit')
 def test_should_use_real_time():
     frozen = datetime.datetime(2015, 3, 5)
     expected_frozen = 1425513600.0
